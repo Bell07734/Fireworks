3af9e69484b41ed9f7d2ee6d323cedcee8912a2c
Oliver <oliver_m_bell@yahoo.com>
2022-03-31 13:59:21 -0300

    Added random colours

--- sketch.js
+++ sketch.js
@@ -22,9 +22,20 @@ function mousePressed() {
 }
 
 function createFireworks(x, y) {
+  let rand = floor(random(3));
   for (let i = 0; i < 100; i++) {
-    particles.push(new Particle(x, y, "#FF0000"));
-    particles.push(new Particle(x, y, "#FFFF00"));
-    particles.push(new Particle(x, y, "#FFAA00"));
+    if (rand == 0) {
+      particles.push(new Particle(x, y, "#FF0000"));
+      particles.push(new Particle(x, y, "#FFFF00"));
+      particles.push(new Particle(x, y, "#FFAA00"));
+    } else if  (rand == 1){
+      particles.push(new Particle(x, y, "#00FF00"));
+      particles.push(new Particle(x, y, "#00FFFF"));
+      particles.push(new Particle(x, y, "#0000FF"));
+    } else {
+      particles.push(new Particle(x, y, "#FF00AA"));
+      particles.push(new Particle(x, y, "#AA00FF"));
+      particles.push(new Particle(x, y, "#FF00FF"));
+    }
   }
 }
