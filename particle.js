e57b20c0f5c2b1417cffd31185da9aa26452d12b
Oliver <oliver_m_bell@yahoo.com>
2022-04-01 07:53:00 -0300

    Added gravity and semicolons

--- particle.js
+++ particle.js
@@ -2,17 +2,21 @@ class Particle {
     constructor(x, y, c) {
         this.pos = createVector(x, y);
         this.vel = p5.Vector.random2D();
-        this.vel.mult(width / random(100,200))
-        this.colour = c;
+        this.vel.mult(width / random(100,200));
+        this.gravity = createVector(0,0.1);
         this.center = createVector(x, y);
-        this.minVel = random(0.5,3)
-        this.maxTime = random(70,100)
+
+        this.colour = c;
+
+        this.minVel = random(0.5,3);
+        this.maxTime = random(30,70);
         this.time = 0;
     }
 
     update() {
         this.pos.add(this.vel);
         this.vel.mult(0.95);
+        this.vel.add(this.gravity);
         this.time ++;
     }
     draw() {
@@ -20,7 +24,7 @@ class Particle {
         point(this.pos);
     }
     isMinVel() {
-        return sqrt(sq(this.vel.x) + sq(this.vel.y)) < this.minVel
+        return sqrt(sq(this.vel.x) + sq(this.vel.y)) < this.minVel;
     }
 
     isMaxTime() {
