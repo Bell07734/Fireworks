544d76b29e0c64e52be174e2fc3643fe01696768
Bell07734 <oliver_m_bell@yahoo.com>
2022-04-15 09:37:51 +0100

    Cleaned up code

--- particle.js
+++ particle.js
@@ -6,14 +6,14 @@ class Particle {
     constructor(x, y, c) {
         this.pos = createVector(x, y);
         this.vel = p5.Vector.random2D();
-        this.vel.mult(width / random(100,200));
-        this.gravity = createVector(0,0.1);
+        this.vel.mult(width / random(100, 200));
+        this.gravity = createVector(0, 0.1);
         this.center = createVector(x, y);
 
         this.colour = c;
 
-        this.minVel = random(0.5,3);
-        this.maxTime = random(30,70);
+        this.minVel = random(0.5, 3);
+        this.maxTime = random(30, 70);
         this.time = 0;
     }
 
@@ -21,7 +21,7 @@ class Particle {
         this.pos.add(this.vel);
         this.vel.add(this.gravity);
         this.vel.mult(0.95);
-        this.time ++;
+        this.time++;
     }
     draw() {
         stroke(this.colour);
